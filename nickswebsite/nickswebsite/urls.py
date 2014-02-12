1d7c198e8372d6ce16689a3bd9cb9afea77540b4
NickStefan <NickStefan12@gmail.com>
2014-02-11 17:20:42 -0800

    added templates

--- nickswebsite/nickswebsite/urls.py
+++ nickswebsite/nickswebsite/urls.py
@@ -7,6 +7,15 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'nickswebsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'blog.views.index'),
+    
+    url(r'^blog/view/(?P<slug>[^\.]+)'),
+    'blog.views.view_post',
+    name='view_blog_post'),
+    
+    url(r'^blog/category/(?P<slug>[^\.]+)',
+    'blog.views.view_category',
+    name='view_blog_category'),
+    
     url(r'^admin/', include(admin.site.urls)),
 )
