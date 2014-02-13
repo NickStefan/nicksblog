c6a661078bc15b7f7a6023b44f5df969c7de6a73
NickStefan <NickStefan12@gmail.com>
2014-02-13 13:34:47 -0800

    trying some css

--- nickswebsite/nickswebsite/urls.py
+++ nickswebsite/nickswebsite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = patterns('',
