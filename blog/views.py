9ae83f27d42c6379cac4653c281fa7a9abb89387
NickStefan <NickStefan12@gmail.com>
2014-09-07 20:52:05 +0000

    Fixed duplicate categories query on homepage
    added a distinct() to the query set for blog categories:
    Categories.objects.distinct().filter(Blog__live=True)
    published a new blog post (changes the sqlite db)

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 def index(request):
     return render_to_response('index.html', {
-        'categories': Category.objects.filter(blog__live=True),
+        'categories': Category.objects.distinct().filter(blog__live=True),
         'posts': Blog.objects.filter(live=True)[:10:-1],
     })
 
@@ -14,7 +14,7 @@ def view_post(request, slug):
     return render_to_response('blog/view_post.html',{
         'post': get_object_or_404(Blog, slug=slug)
     })
-    
+
 @staff_member_required
 def preview_post(request, slug):
     return render_to_response('blog/view_post.html',{
@@ -30,7 +30,7 @@ def view_category(request, slug):
 
 def view_about(request):
     return render_to_response('about.html',)
-    
+
 def search(request):
     errors = []
     if 'q' in request.GET:
@@ -45,14 +45,13 @@ def search(request):
             return render_to_response('blog/search_results.html', {
             'error': errors
             })
-            
+
         else:
             return render_to_response('blog/search_results.html',{
-            'foundposts': Blog.objects.filter(live=True).filter(body__icontains=q)[::-1], 
+            'foundposts': Blog.objects.filter(live=True).filter(body__icontains=q)[::-1],
             'query': q
         })
     else:
         return render_to_response('blog/search_results.html', {
         'error': errors
     })
-    
